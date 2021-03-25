38c5d50119daa6ff129a994b8cd4ea24a88f1d78
Matheus Martino <matheus.martino97@gmail.com>
2021-03-25 17:09:46 -0300

    Adding RegisteredCustomers page.

---
+++ src/pages/Register.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import registerCustomerAction from '../actions';
+import { connect } from 'react-redux';
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+
+  render() {
+    const { registerCustomer } = this.props;
+    const { email, password } = this.state
+
+    return (
+      <div>
+        <div className="inputs" onChange={this.handleChange}>
+          <label htmlFor="email">
+            Email:
+            <input type="text" placeholder="Digite seu email" id="email" value={ this.state.email } />
+          </label>
+          <br />
+          <label htmlFor="password">
+            Senha:
+            <input type="text" placeholder="Digite sua senha" id="password" value={ this.state.password } />
+          </label>
+          <br />
+          <button
+            type="button"
+            onClick={() => {
+              registerCustomer(this.state);
+
+              this.setState({
+                email: '',
+                password: ''
+              })
+            }}
+          >
+            Cadastrar
+          </button>
+        </div>
+        <Link to="/">Home</Link>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    registerCustomer: (customerData) =>
+      dispatch(registerCustomerAction(customerData)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Register);
